40e9603573721bc0f2faedf6ac2d3fe53701c764
unknown <tigric@gmail.com>
2020-04-20 15:30:47 -0400

    not completed yet

---
+++ basicAlgorithms/factorize.js
@@ -0,0 +1,12 @@
+function factorialize(num) {
+
+    let result = 1;
+    
+    for (var i = num-1; i > 0; i--) {
+      num = num * i;
+      result = num;
+      }
+      return result;
+    }
+    
+    factorialize(3);
