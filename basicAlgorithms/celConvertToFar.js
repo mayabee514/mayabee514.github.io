676d69dea8c8ee9f727ac89c7adc919e8ab51614
unknown <tigric@gmail.com>
2020-04-20 10:03:24 -0400

    Adding various Basic Algorithmic Coding section

---
+++ basicAlgorithms/celConvertToFar.js
@@ -0,0 +1,9 @@
+//Convert Celsius to Fahrenheit
+
+  function convertToF(celsius) {
+    let fahrenheit = 32 + (celsius * 9)/5;
+    
+    return console.log(fahrenheit);
+  }
+console.log(convertToF(30));
+
